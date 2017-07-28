d25f0ad0acc277dcbd2d40be25fd13e70eae6158
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-28 12:08:20 -0400

    finished maqueta, starting with js

---
+++ assets/js/javascript.js
@@ -0,0 +1,7 @@
+/*arregloObjetos.search_results.forEach(function(el){
+
+	document.write("<img src='" + el.listing.thumbnail_url + "'" + "><br>");
+	document.write("<b>" + el.listing.localized_city + " - " + el.listing.name + "</b><br>");
+	document.write("Baños : " + el.listing.bathrooms + "<br>");
+	document.write("Dormitorios : " + el.listing.bedrooms + "<br>");
+})*/
--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,20 @@ $(document).ready(function () {
     //Side nav bar
     $(".button-collapse").sideNav();
     console.log("funciona");
-
     $('.container').masonry({
         // options
         itemSelector: '.element',
-        columnWidth: 200
     });
-
+    jsonArray();
 });
+
+function jsonArray(){
+    arregloObjetos.search_results.forEach(function(el){
+    console.log(el.id);
+	/*document.write("<img src='" + el.listing.thumbnail_url + "'" + "><br>");
+	document.write("<b>" + el.listing.localized_city + " - " + el.listing.name + "</b><br>");
+	document.write("Baños : " + el.listing.bathrooms + "<br>");
+	document.write("Dormitorios : " + el.listing.bedrooms + "<br>");*/
+})
+}
 
