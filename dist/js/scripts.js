21d98a2cc8849fdbab72727cd80e22294859454e
“Laura <laureen.pelaez.isaza@gmail.com>
2017-07-31 11:24:40 -0400

    finished maqueta and styles, working modal

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -20262,49 +20262,84 @@ var arregloObjetos= {"search_results":[{"id":1,"title":"Fuke zisukje bu dibas si
 	document.write("Dormitorios : " + el.listing.bedrooms + "<br>");
 })*/
 $(document).ready(function () {
-    //Side nav bar
     $(".button-collapse").sideNav();
     $('.modal').modal();
-
+    $(".button-collapse").sideNav();
+    $('select').material_select();
     jsonArray();
 });
 
-
+/*Creating the html with the forEach Method at the Json Object*/
 function jsonArray() {
     var html = "";
     arregloObjetos.search_results.forEach(function (el) {
         html += ` 
                     <div class="pin">
-                            <img id="photo`+ el.id + `"` + `" src="dist/img/` + el.image_url + `">
-                            <p class="title">`+ el.title + `<i class="fa fa-pinterest" aria-hidden="true"></i></p>
+                            <a href="#modal" onclick="onClickModal(this)" data-id="` + el.id + `" class="modal-trigger" id="pinterest_` + el.id + `"> 
+                            <img  id="photo`+ el.id + `"` + `" src="dist/img/` + el.image_url + `"></a>
+                            <p class="title">`+ el.title + `<i class="fa fa-thumb-tack" aria-hidden="true"></i><i>36,6k</i><i class="fa fa-check" aria-hidden="true"></i>
+                            <i>6</i>    </p>
                             <p class="description">`+ el.description + `</p>
-                            <p class="hasthag"><i class="fa fa-meetup" aria-hidden="true"></i>` + el.username + ` #  ` + el.hashtag + `</p>
-                             
-                    </div>`;
+                            <p class="hasthag"><a class=logoM>M</a>` + el.username + ` #  ` + el.hashtag + `</p>                              
+                    </div>`
+
+
         $('#columns').html(html);
-
-
-
     })
 }
-/*function jsonArray() {
-    var html = "";
-    arregloObjetos.search_results.forEach(function (el) {
-        html += ` <div class="element">
-                            <img id="photo`+ el.id + `"`+`" src="dist/img/`+ el.image_url + `">
-                            <div class="text">
-                            <h6 class="title">`+ el.title +  `<i class="fa fa-pinterest" aria-hidden="true"></i></h6>
-                            <p class="description">`+ el.description + `</p>
-                            <p class="hasthag"><i class="fa fa-meetup" aria-hidden="true"></i>` + el.username +  ` #  `+ el.hashtag + `</p>
-                            </div>            
-                    </div>`;
-        $('.contenedor').html(html);
 
+/*Creating the modal*/
+function onClickModal(element) {
+    console.log(element);
+    var id = $("#" + element.id).attr('data-id');
+    console.log(id);
+        arregloObjetos.search_results.forEach(function (el) {
+        if (el.id == id) {
+            var html = "";
+            html += `
+                        <div class="row upper-content">
+                        <div class="col s6 left">
+                            <i class="fa fa-upload" aria-hidden="true"></i>
+                            <i class="fa fa-check" aria-hidden="true"></i>
+                            <i>...</i>
+                        </div>
+                        <div class="col s6 right">
+                             <a class="right"><h6><i class="fa fa-thumb-tack" aria-hidden="true"></i>Guardar</h6></a>
+                        </div>
+                        </div>
+                <div class="modal-content">
+                        <h4>`+ el.title + `</h4>
+                        <img src="dist/img/` + el.image_url + `">
+                </div>
+                <div class="modal-footer">
+                    <div class="row upper-content">
+                        <div class="col s6 left">
+                            <i class="fa fa-upload" aria-hidden="true"></i>
+                            <i class="fa fa-check" aria-hidden="true"></i>
+                            <i>...</i>
+                        </div>
+                        <div class="col s6 right">
+                         <a class="right modal-action modal-close"><h6><i class="fa fa-thumb-tack" aria-hidden="true"></i>Guardar</h6></a>
+                        </div>
+                    </div>
+                    <div class="row final-footer">
+                        <div class="col s6">
+                            <h1 class="hasthag"><a class=logoM>M</a>` + el.username + ` #  ` + el.hashtag + `</h1> 
+                        </div>
+                        <div class="col s6">
+                            <h2>Leerlo</h2>
+                    </div>
+                </div>
+                 <div class="row">
+                    <div class="col s12">
+                    <p class="description">`+ el.description + `</p>
+                    </div>
+                 </div>
+                
+            </div>`;
 
-
-    })
-}*/
-
-
-
+            $(".modal").html(html);
+        }
+    });
+}
 
